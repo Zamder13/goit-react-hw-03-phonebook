a57b080ffc7a66eec15c0a45289b1df2dee37b2a
Zamder13 <Zamder@ukr.net>
2022-01-29 09:20:00 +0200

    commit to main

---
+++ src/components/app/App.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import Form from "../contactForm/ContactForm";
+import ContactList from "../contactsList/ContactList";
+import Filter from "../filter/Filter";
+import { nanoid } from "nanoid";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+
+  formSubmit = (newContactSubmit) => {
+    this.setContact(newContactSubmit);
+  };
+
+  setContact = async (text) => {
+    const { contacts } = this.state;
+
+    if (
+      contacts.find(
+        (contact) => contact.name.toLowerCase() === text.name.toLowerCase()
+      )
+    ) {
+      alert(`${text.name} is already in contacts.`);
+    } else {
+      const contact = {
+        id: nanoid(),
+        name: text.name,
+        number: text.number,
+      };
+      this.setState(({ contacts }) => ({
+        contacts: [contact, ...contacts],
+      }));
+    }
+  };
+
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactId
+      ),
+    }));
+  };
+
+  changeFilter = (event) => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  getfilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+
+    const filteredContacts = this.getfilteredContacts();
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <Form onSubmit={this.formSubmit} />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          persons={filteredContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </>
+    );
+  }
+}
+export default App;
---
+++ src/components/contactForm/ContactForm.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { FormTable, AddButton, Wrapper } from "./ContactForm.styled.jsx";
+
+class Form extends Component {
+  state = { name: "", number: "" };
+
+  handleChangeinput = (event) => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: "", number: "" });
+  };
+
+  render() {
+    const data = this.state;
+
+    return (
+      <FormTable onSubmit={this.handleSubmit} className="form">
+        <Wrapper>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={data.name}
+              onChange={this.handleChangeinput}
+            />
+          </label>
+
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={data.number}
+              onChange={this.handleChangeinput}
+            />
+          </label>
+        </Wrapper>
+
+        <AddButton type="submit">Add contact</AddButton>
+      </FormTable>
+    );
+  }
+}
+
+export default Form;
